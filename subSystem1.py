2fdb63294bc859c2617adc53070d19a9bfa6d453
Arash Azma <arashazma8@gmail.com>
2025-01-10 21:45:52 +0330

    Implement structure of weighted round robin

--- subSystem1.py
+++ subSystem1.py
@@ -19,29 +19,31 @@ index_to_ready_queue = {
     2: ready_queue3
 }
 
-QUANTUM = 0.5
+QUANTUM = 1
 
 core_load = [0, 0, 0]
-core_weights = [1, 1, 1]
 
-def weighted_round_robin(task: Task):
+def get_quantum(task: Task):
+    return QUANTUM * task.weight
+
+def weighted_round_robin(task: Task, is_first):
     global core_load
-    min_load = float('inf')
-    chosen_core = -1
+    
+    if is_first:
+        ready_queue = index_to_ready_queue[int(task.dest_cpu) - 1]
+        ready_queue.put(task)
+        core_load[int(task.dest_cpu) - 1] += 1
+        task.state = 'ready'
+    else:
+        chosen_core = core_load.index(min(core_load))
 
-    for i in range(3):
-        effective_load = core_load[i] / core_weights[i]
-        if effective_load < min_load:
-            min_load = effective_load
-            chosen_core = i
-
-    core_load[chosen_core] += 1
-    index_to_ready_queue[chosen_core].put(task)
-    task.state = 'ready'
-    print(f"Task {task.name} assigned to core {chosen_core}")
-    return chosen_core
+        core_load[chosen_core] += 1
+        print(f"\nTask {task.name} assigned to core {chosen_core}")
+        index_to_ready_queue[chosen_core].put(task)
+        task.state = 'ready'
 
 def core(index, resources: List[Resource_]):
+    global waiting_queue, alive_tasks
     ready_queue = index_to_ready_queue[index]
     
     while True:
@@ -50,44 +52,48 @@ def core(index, resources: List[Resource_]):
             R2 = resources[1]
             task: Task = ready_queue.get(timeout=1) 
             task.state = 'running'
-            print(f'\nR usage {task.resource1_usage, task.resource2_usage} count {R1.count, R2.count}\n')
-            # check if resources are available
             if task.resource1_usage <= R1.count and task.resource2_usage <= R2.count:
                 R1_lock.acquire()
                 R2_lock.acquire()
-                print(f"Task {task.name} acquired Resources")
-            # if resources are not available, put the task back in the waiting queue
+                print(f"\nTask {task.name} acquired Resources")
             else:
-                print(f"Task {task.name} is waiting for resources")
+                print(f"\nTask {task.name} is waiting for resources")
                 task.state = 'waiting'
                 waiting_queue.put(task)
                 continue
 
-            exec_time = min(QUANTUM, task.remaining_duration)
-            # time.sleep(exec_time)
-            print(f'before exec {task.remaining_duration}')
-            task.remaining_duration -= exec_time
-            print(f'after exec {task.remaining_duration}')
+            exec_time = min(get_quantum(task), task.duration)
+            time.sleep(.5)
+            task.duration -= exec_time
 
             R1_lock.release()
             R2_lock.release()
 
-            if task.remaining_duration > 0:
+            if task.duration > 0:
                 task.state = 'waiting'
-                print(f"Task {task.name} preempted with {task.remaining_duration} time remaining")
-                ready_queue.put(task)
+                print(f"\nTask {task.name} went back to WAITING QUEUE with {task.duration} time remaining")
+                waiting_queue.put(task)
             else:
                 task.state = 'completed'
-                print(f"Task {task.name} completed on core {index}")
+                print(f"\nTask {task.name} COMPLETED on core {index}")
+                alive_tasks -=1
 
+            core_load[index] -= 1
+            
         except Exception as e:
             break
 
 # handle krdn Starvation
 def subSystem1(resources: List[Resource_], tasks: List[Task]):
-    global ready_queue1, ready_queue2, ready_queue3, waiting_queue
-
+    global ready_queue1, ready_queue2, ready_queue3, waiting_queue, alive_tasks
+    
     number_of_tasks = len(tasks)
+    alive_tasks = len(tasks)
+    
+    min_duration = min(task.duration for task in tasks)
+    for task in tasks:
+        task.weight = max(1, task.duration // min_duration)
+    
     for task in tasks:
         waiting_queue.put(task)
 
@@ -98,17 +104,14 @@ def subSystem1(resources: List[Resource_], tasks: List[Task]):
         t.start()
     
     is_first = 0
-    while not waiting_queue.empty():
-        task = waiting_queue.get()
-        
-        if is_first < number_of_tasks:
-            # print(f"Task {task.name} assigned to {int(task.dest_cpu) - 1}")
-            index_to_ready_queue[int(task.dest_cpu) - 1].put(task)
-            task.state = 'ready'
-            is_first += 1
     
-        else:
-            weighted_round_robin(task)
+    while alive_tasks > 0:
+        if not waiting_queue.empty():
+            task = waiting_queue.get()
+            weighted_round_robin(task, is_first < number_of_tasks)
+            is_first += 1
+            
+        time.sleep(0.2)
 
     for t in cores:
         t.join()
