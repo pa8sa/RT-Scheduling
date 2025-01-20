ddae1ba479108efd8a3f119d1c093a0b21b952ae
Arash Azma <arashazma8@gmail.com>
2025-01-20 16:34:04 +0330

    fix some bugs and add preepmtion

--- subSystem3.py
+++ subSystem3.py
@@ -6,20 +6,30 @@ from task import Task
 from resource_ import Resource_
 
 time_unit = -1
+
+def increment_time_unit():
+    global time_unit
+    time_unit+=1
+    
+start_barrier = threading.Barrier(1, increment_time_unit)
+
+r_lock = threading.Lock()
 ready_queue = []
-all_queue = []
 tasks_global = []
 
 '''
         duration  resource1_usage  resource2_usage  entering_time   period  recursion
-    T31     2           2               3                0            10       1
-    T32     1           2               3                0            5        2
+    T31     2           2               3                0            10        2
+    T32     1           2               3                0            5         2
     
 '''
 
 def add_to_ready_queue(task: Task):
-    ready_queue.append(task)
-    ready_queue.sort(key=lambda task: task.priority, reverse=True)
+    with r_lock:
+        if (task.name  in [t.name for t in ready_queue]):
+            return
+        ready_queue.append(task)
+        ready_queue.sort(key=lambda task: task.priority, reverse=True)
 
 def is_schedulable(tasks: List[Task]):
     sorted_tasks = sorted(tasks, key=lambda task: task.period)
@@ -32,60 +42,92 @@ def is_schedulable(tasks: List[Task]):
     return True
 
 def check_for_tasks():
-    print(f'\n[TASKS] {[(task.name, task.duration, task.entering_time) for task in tasks_global]}\n')
+    print(f'\n[GLOBAL TASKS] {[(task.name, task.duration, task.entering_time, task.state) for task in tasks_global]}\n')
+    
     for t in tasks_global:
-        if t.entering_time <= time_unit and t.duration > 0:
-            print(f'\n ADDED TO QUEUE {t.entering_time} {t.duration}\n')
+        if (t.entering_time <= time_unit and 
+            t.state != 'completed' and 
+            t.state != 'running' and 
+            t.name not in [task.name for task in ready_queue]):
+            
+            # print(f'\n ADDED TO QUEUE TIME: {time_unit} - {t.name} {t.entering_time} {t.duration}\n')
             add_to_ready_queue(copy.deepcopy(t))
+    
+    for t in ready_queue:
+        if t.entering_time > time_unit:
+            with r_lock:
+                ready_queue.pop(t) 
+            
+def add_to_global(task: Task):
+    global tasks_global
+    for i, t in enumerate(tasks_global):
+        if t.name == task.name:
+            task.duration = t.duration
+            tasks_global[i] = copy.deepcopy(task)
+            break
+    
 
 def core(resources: List[Resource_]):
     prev_task = None
     while True:
-        
-        if time_unit > 10:
+        start_barrier.wait()
+        print(f'========================================================================================================= time_unit {time_unit}')
+        if time_unit > 15:
             break
         
         check_for_tasks()
         
-        if (len(ready_queue) == 0 and prev_task is None):
-            continue
+        with r_lock:
+            for i, t in enumerate(ready_queue):
+                if prev_task and t.name == prev_task.name:
+                    ready_queue.pop(i)
+                    break
+            print(f'\n[READY QUEUE] {[(task.name, task.duration, task.entering_time, task.state) for task in ready_queue]}\n')
         
+            if (len(ready_queue) == 0 and prev_task is None):
+                continue
+            
         if prev_task is not None:
-            task = prev_task
+            if len(ready_queue) > 0 and ready_queue[0].priority >= prev_task.priority:
+                task = ready_queue.pop(0)
+                print(f'\n[PREEMPTION] {task.name} preempted {prev_task.name} \n')
+                prev_task.state = 'ready'
+                add_to_ready_queue(prev_task)
+                prev_task = None
+            else:
+                task = prev_task
         else:
             task = ready_queue.pop(0)
             
-        print(f'\n[RUNNING] TIME: {time_unit} - {task.name} {task.duration} \n')
-        
         task.state = 'running'
+        print(f'\n[RUNNING] TIME: {time_unit} - {task.name} duration: {task.duration}  recursion: {task.recursion} state: {task.state}\n')
+        
         task.duration -= 1
-        task.recursion -= 1
         
         if task.duration == 0:
+            task.recursion -= 1
             if task.recursion >= 1:
                 task.entering_time = ((time_unit // task.period) + 1) * task.period
                 task.state = 'ready'
+                add_to_global(task)
                 
-                for i, t in enumerate(tasks_global):
-                    if t.name == task.name:
-                        tasks_global.pop(i)
-                        task.duration = t.duration
-                        tasks_global.append(copy.deepcopy(task))
-                        print(f'\n[WENT FOR NEXT ROUND] TIME:{time_unit} {t.duration} {task.entering_time}\n')
+                print(f'\n[WENT FOR NEXT RECURSION] TIME:{time_unit} {task.name}  duration: {task.duration} entering_time: {task.entering_time} {task.state}\n')
             else:
-                for i, t in enumerate(tasks_global):
-                    if t.name == task.name:
-                        tasks_global.pop(i)
                 task.state = 'completed'
-                print(f'\n[COMPLETED] TIME: {time_unit} {task.name} {task.duration} \n')
+                add_to_global(task)
+                
+                print(f'\n[COMPLETED] TIME: {time_unit} {task.name} duration: {task.duration} {task.state}\n')
                 
             prev_task = None
         else:
+            print(f'\n[WENT FOR NEXT ROUND] TIME:{time_unit} {task.name}  duration: {task.duration} entering_time: {task.entering_time} {task.state}\n')
             prev_task = task
             
             
 def subSystem3(resources: List[Resource_], tasks: List[Task]):
     global time_unit, tasks_global
+    # print(is_schedulable(tasks))
+    # return 
     
     t = threading.Thread(target=core, args=(resources,))
     t.start()
@@ -96,12 +138,5 @@ def subSystem3(resources: List[Resource_], tasks: List[Task]):
     tasks.sort(key=lambda task: task.priority, reverse=True)
     
     tasks_global = copy.deepcopy(tasks)
-    
-    while True:
-        time_unit+=1
-        print(f'========================================================================================================= time_unit {time_unit}')
-    
-        if time_unit > 10:
-            break
     
     t.join()
