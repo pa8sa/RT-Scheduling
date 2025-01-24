12c7a6664f0f031aa2446ecd48cd85d0b2443d2a
Arash Azma <arashazma8@gmail.com>
2025-01-24 18:41:41 +0330

    fix system1

--- subSystem1.py
+++ subSystem1.py
@@ -4,6 +4,7 @@ from typing import List
 from queue import Queue
 from task import Task
 from resource_ import Resource_
+import copy
 
 glob_task1: Task = None
 glob_task2: Task = None
@@ -33,25 +34,63 @@ def wait_for_print():
     globals.print_turn += 1
     globals.print_turn_lock.release()
 
+def add_task_to_ready_queue(task: Task, is_first):
+    if is_first:
+        ready_queue = index_to_ready_queue[int(task.dest_cpu) - 1]
+        ready_queue.put(task)
+        task.state = 'ready'
+    else:
+        queue_sizes = [sum(task.duration for task in list(index_to_ready_queue[i].queue)) for i in range(3)]
+        chosen_core = queue_sizes.index(min(queue_sizes)) 
+        
+        index_to_ready_queue[chosen_core].put(task)
+        task.state = 'ready'
+
+def add_to_waiting_queue(task: Task):
+    global waiting_queue
+    temp_list = []
+    while not waiting_queue.empty():
+        temp_list.append(waiting_queue.get())
+
+    temp_list.append(task)
+
+    temp_list.sort(key=lambda t: (t.age + t.entering_time))
+
+    for sorted_task in temp_list:
+        waiting_queue.put(sorted_task)
+
+def read_from_waiting_queue():
+    global waiting_queue, number_of_tasks, is_first
+    
+    while not waiting_queue.empty():
+        task = waiting_queue.get()
+        if task.entering_time <= globals.time_unit+1 and task.duration > 0:
+            add_task_to_ready_queue(task, is_first < number_of_tasks)
+            is_first += 1
+        else:
+            add_to_waiting_queue(task)
+
 def print_output():
-    global glob_task1, glob_task2, glob_task3, ready_queue1, ready_queue2, ready_queue3, waiting_queue, completed_tasks
+    global glob_task1, glob_task2, glob_task3, ready_queue1, ready_queue2, ready_queue3, waiting_queue, completed_tasks, number_of_tasks
     print("------------------------------------------------- time unit:", globals.time_unit, "-------------------------------------------------")
     print("Sub1:")
     print(f"\tR1: {globals.sub1_resources[0].count if globals.sub1_resources[0] else '-'} R2: {globals.sub1_resources[1].count if globals.sub1_resources[1] else '-'}")
-    print(f"\tWaiting Queue: {[task.name if task.entering_time<= globals.time_unit else '' for task in list(waiting_queue.queue)]}")
+    print(f"\tWaiting Queue: {[task.name if task.entering_time<= globals.time_unit and task.state == 'waiting' else '' for task in list(waiting_queue.queue)]}")
     
     print(f"\tCore1:")
-    print(f"\t\tRunning Task: {glob_task1.name if glob_task1 else 'idle'}")
+    print(f"\t\tRunning Task: {glob_task1.name  if glob_task1 and glob_task1.state=='running' else 'idle'}")
     print(f"\t\tReady Queue: {[task.name if task.state=='ready' else '' for task in list(ready_queue1.queue)]}")
     
     print(f"\tCore2:")
-    print(f"\t\tRunning Task: {glob_task2.name if glob_task2 else 'idle'}")
+    print(f"\t\tRunning Task: {glob_task2.name if glob_task2 and glob_task2.state=='running' else 'idle'}")
     print(f"\t\tReady Queue: {[task.name if task.state=='ready' else '' for task in list(ready_queue2.queue)]}")
     
     print(f"\tCore3:")
-    print(f"\t\tRunning Task: {glob_task3.name if glob_task3 else 'idle'}")
+    print(f"\t\tRunning Task: {glob_task3.name if glob_task3 and glob_task3.state=='running' else 'idle'}")
     print(f"\t\tReady Queue: {[task.name if task.state=='ready' else '' for task in list(ready_queue3.queue)]}")
     print(f"\tCompleted Tasks: \n\t\t{[task.name if task.state=='completed' else '' for task in list(completed_tasks)]}")
+    
+    read_from_waiting_queue()
 
 finish_barrier = threading.Barrier(3, action=wait_for_print)
 
@@ -65,18 +104,6 @@ QUANTUM = 1
 
 def get_quantum(task: Task):
     return QUANTUM * task.weight
-
-def add_task_to_ready_queue(task: Task, is_first):
-    if is_first:
-        ready_queue = index_to_ready_queue[int(task.dest_cpu) - 1]
-        ready_queue.put(task)
-        task.state = 'ready'
-    else:
-        queue_sizes = [sum(task.duration for task in list(index_to_ready_queue[i].queue)) for i in range(3)]
-        chosen_core = queue_sizes.index(min(queue_sizes)) 
-        
-        index_to_ready_queue[chosen_core].put(task)
-        task.state = 'ready'
 
 def push_migration():
     global ready_queue1, ready_queue2, ready_queue3
@@ -160,7 +187,7 @@ def core(index):
             else:
                 task = prev_task
                 task.state = 'running'
-                
+            
             # print (f"\n [RUNNING] Task {task.name} is RUNNING on core {index}")
             with globals.sub1_resource_lock:
                 if task.resource1_usage <= R1.count and task.resource2_usage <= R2.count:
@@ -192,11 +219,11 @@ def core(index):
             how_many_rounds -= 1
             
             if index == 0:
-                glob_task1 = task
+                glob_task1 = copy.deepcopy(task)
             elif index == 1:
-                glob_task2 = task
+                glob_task2 = copy.deepcopy(task)
             elif index == 2:
-                glob_task3 = task
+                glob_task3 = copy.deepcopy(task)
 
             with globals.sub1_resource_lock:
                 R1.count += task.resource1_usage
@@ -235,19 +262,6 @@ def core(index):
             print(f'{index} EXCEPTION:  {e}')
             break
 
-def add_to_waiting_queue(task: Task):
-    global waiting_queue
-    temp_list = []
-    while not waiting_queue.empty():
-        temp_list.append(waiting_queue.get())
-
-    temp_list.append(task)
-
-    temp_list.sort(key=lambda t: (t.age + t.entering_time))
-
-    for sorted_task in temp_list:
-        waiting_queue.put(sorted_task)
-
 def increment_tasks_age():
     global waiting_queue
 
@@ -262,7 +276,7 @@ def increment_tasks_age():
         add_to_waiting_queue(task)
 
 def subSystem1(tasks: List[Task]):
-    global ready_queue1, ready_queue2, ready_queue3, waiting_queue, alive_tasks
+    global ready_queue1, ready_queue2, ready_queue3, waiting_queue, alive_tasks, number_of_tasks, is_first
     
     number_of_tasks = len(tasks)
     alive_tasks = len(tasks)
@@ -278,25 +292,27 @@ def subSystem1(tasks: List[Task]):
         cores.append(t)
         t.start()
     
-    is_first = 0
-    
     for task in tasks:
         waiting_queue.put(task) 
         
-    while True:
-        with alive_lock:
-            if alive_tasks == 0:
-                # print(f'\n +++++++++++++++++++++++++++ [FINISHED] subsystem 1')
-                break
+    is_first = 0
+    
+    read_from_waiting_queue()
+    
+    # while True:
+    #     with alive_lock:
+    #         if alive_tasks == 0:
+    #             # print(f'\n +++++++++++++++++++++++++++ [FINISHED] subsystem 1')
+    #             break
             
-        if not waiting_queue.empty():
-            task:Task = waiting_queue.get()
+        # if not waiting_queue.empty():
+        #     task:Task = waiting_queue.get()
             
-            if task.entering_time <= globals.time_unit and task.duration > 0:
-                add_task_to_ready_queue(task, is_first < number_of_tasks)
-                is_first += 1
-            else:
-                add_to_waiting_queue(task)
+        #     if task.entering_time <= globals.time_unit and task.duration > 0:
+        #         add_task_to_ready_queue(task, is_first < number_of_tasks)
+        #         is_first += 1
+        #     else:
+        #         add_to_waiting_queue(task)
             
     for t in cores:
         t.join()
